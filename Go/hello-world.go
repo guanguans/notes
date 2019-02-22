5bb7f6247ee243ef03425b320f5ea0ee4b6e13a5
guanguans <798314049@qq.com>
2019-02-22 10:59:03 +0800

    docs(Go): 添加 hello-world

---
+++ Go/hello-world.go
@@ -0,0 +1,11 @@
+/**.-----------------------------------------------
+ * |   Author: guanguans <yzmguanguan@gmail.com>
+ * '---------------------------------------------*/
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello World")
+}
